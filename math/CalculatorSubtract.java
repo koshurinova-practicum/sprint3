a69432681ab1ad0a79c564856a4a111982c49fbf
koshurinova <nothern_girl@mail.ru>
2026-03-25 19:49:35 +0300

    YНовый метод

---
+++ math/CalculatorSubtract.java
@@ -0,0 +1,11 @@
+package math;
+
+public class CalculatorSubtract {
+
+    // Метод для вычитания двух целых чисел
+    public int subtract(int a, int b) {
+        System.out.println("текст");
+        return a - b;
+    }
+}
+
